890b28b499858de0e087f91bff96bdb81894bb52
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2021-03-15 18:19:14 +0200

    adding project

---
+++ src/Results.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h2>No Pets Found</h2>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city},${pet.state}`}
+            key={pet.id}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
